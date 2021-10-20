b8cd830bd2960ec155a78d893d0e2c301007080d
jhdl0157 <jhdl0157@naver.com>
2021-10-20 15:55:27 +0900

    1919번풀이

--- CodingTest/CodingTest/1919.cpp
+++ CodingTest/CodingTest/1919.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+int arr[2][26], res;
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -8,5 +9,16 @@ int main(void) {
 	string a, b;
 	cin >> a >> b;
 
+	for (char c : a) {
+		arr[0][c - 'a']++;
+	}
 
+	for (char c : b) {
+		arr[1][c - 'b']++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		res += abs(arr[0][i] - arr[1][i]);
+	}
+	cout << res;
 }
